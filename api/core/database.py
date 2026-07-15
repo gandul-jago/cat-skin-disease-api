991cfee7fadd4fd924afe9b82256a0b358a8d4d6
gandul-jago <emailgithubkamu@example.com>
2026-07-15 10:51:29 +0700

    databse ubah

--- api/core/database.py
+++ api/core/database.py
@@ -5,13 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql://{os.getenv('PGUSER')}:"
-    f"{os.getenv('PGPASSWORD')}@"
-    f"{os.getenv('PGHOST')}:"
-    f"{os.getenv('PGPORT')}/"
-    f"{os.getenv('PGDATABASE')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
