e0caec9202f7e63499b5a9889d76fbc86a9d3178
gandul-jago <emailgithubkamu@example.com>
2026-07-15 10:47:27 +0700

    databse ubah

--- api/core/database.py
+++ api/core/database.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = (
-    f"postgresql://{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}:"
-    f"{os.getenv('DB_PORT')}/"
-    f"{os.getenv('DB_NAME')}"
+    f"postgresql://{os.getenv('PGUSER')}:"
+    f"{os.getenv('PGPASSWORD')}@"
+    f"{os.getenv('PGHOST')}:"
+    f"{os.getenv('PGPORT')}/"
+    f"{os.getenv('PGDATABASE')}"
 )
 
 engine = create_engine(DATABASE_URL)
